745d584ce5bd8a043276401eeb696a34edc781f5
S. Jansen <sa.jansen1@gmail.com>
2022-12-30 14:34:01 +0200

    fix(release): Disabled commit as there will be nothing to commit.

--- release.sh
+++ release.sh
@@ -3,14 +3,13 @@
 function release {
   ./docker.sh build || exit 1
   ./docker.sh push || exit 1
-  
+
   RELEASE_VERSION=$(git rev-list HEAD --first-parent --count)
   echo "Release version: ${RELEASE_VERSION}"
-  
-  git commit -m "version release: ${RELEASE_VERSION}" || exit 1
+
+#  git commit -m "version release: ${RELEASE_VERSION}"
   git tag "v${RELEASE_VERSION}" || exit 1
   git push -u origin master --tags || exit 1
 }
 
 release
-
